ac1882ab19cf6bc555a7cfd95abf18c5d9670296
johnkarlmolina <molina.johnkarl.ponteras@gmail.com>
2026-03-19 15:14:03 +0800

    replace url on download button

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -124,7 +124,7 @@ export default function Download() {
 							{/* Action Buttons */}
 							<div>
 								<a
-								href="https://drive.google.com/uc?export=download&id=185_gpZ9uvlzs4amU554D86cCRIwTvxel	"
+								href="https://mega.nz/file/ywpA0Y6L#IidOszZ0N4xMYBZ8i2g1M0rPbzKSTcFZf2MfWz6VQxw	"
 								target="_blank"
 								rel="noopener noreferrer"
 									className="inline-flex items-center gap-2 bg-teal-500 text-white px-6 py-3 rounded-full font-semibold hover:bg-teal-600 transition-colors shadow-lg"
